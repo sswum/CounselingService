183bbb69178b5140842b2e588376f7dfddd3a2c4
jek730 <jejek730@gmail.com>
2024-09-06 20:01:42 +0900

    Swagger API 추가

--- src/main/java/xyz/sangdam/file/repositories/FileInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.sangdam.file.repositories;
-
-import xyz.sangdam.file.entities.FileInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
-
-}
--- src/main/java/xyz/sangdam/global/configs/SwaggerConfig.java
+++ src/main/java/xyz/sangdam/global/configs/SwaggerConfig.java
@@ -6,14 +6,14 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(info=@Info(title="파일 API", description = "파일업로드, 다운로드, 삭제, 조회 기능 제공"))
+@OpenAPIDefinition(info=@Info(title="상담 API", description = "상담 기능 정의 명세"))
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi apiGroup() {
         return GroupedOpenApi.builder()
-                .group("파일 API v1")
+                .group("상담 API v1")
                 .pathsToMatch("/**")
                 .build();
     }
